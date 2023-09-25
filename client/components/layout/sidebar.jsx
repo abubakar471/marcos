302975b4b9f0a8f8193a8b57199c22884750de82
abubakar471 <abdurjoy2001@gmail.com>
2023-09-25 18:32:08 +0600

    sidebar fixed

--- client/components/layout/sidebar.jsx
+++ client/components/layout/sidebar.jsx
@@ -5,9 +5,12 @@ const Sidebar = ({ active, setActive }) => {
   return (
     <div className="w-full h-[90vh] flex flex-col bg-blue-500 shadow border-r">
       <div className="w-full grow ">
-        <div className=" bg-blue-400 text-white p-4 flex items-center gap-x-2">
-          <MdSecurity size={30} className="block" />
-          <h2 classNane="hidden md:block lg:block xl:block">Password Manager</h2>
+        <div className=" bg-blue-400 text-white p-4 flex items-center justify-center
+         md:justify-start lg:justify-start xl:justify-start gap-x-2">
+          <div className="flex items-center justify-center">
+            <MdSecurity size={30} className="block" />
+          </div>
+          <h2 className="hidden md:block lg:block xl:block">Password Manager</h2>
         </div>
       </div>
 
