6023ddc20b51a98207967724b122cea0607fe869
abubakar471 <abdurjoy2001@gmail.com>
2023-09-27 12:22:12 +0600

    updated init vector to plain text

--- server/controllers/password.js
+++ server/controllers/password.js
@@ -3,7 +3,7 @@ const crypto = require("crypto");
 const algorithm = "aes-256-cbc";
 // generate 16 bytes of random data
 // const initVector = crypto.randomBytes(16);
-const initVector = Buffer.from("modernDaymorni9ngCla$h", "hex");
+const initVector = "modernDaymorni9ngCla$h";
 // secret key generate 32 bytes of random data
 const securityKey = process.env.CRYPTO_SECRET;
 
