3eee0e01eb6a162e509268ff1a5b33a461f27e4c
abubakar471 <abdurjoy2001@gmail.com>
2023-09-27 13:23:10 +0600

    added text area in edit and show modal

--- client/components/modal/edit-password-modal.jsx
+++ client/components/modal/edit-password-modal.jsx
@@ -1,5 +1,6 @@
 import { RxCross1 } from "react-icons/rx";
 import { TextField } from "@mui/material";
+import { TextareaAutosize } from "@mui/base";
 
 const EditPasswordModal = ({ open, setOpen,
     title, setTitle, platform, setPlatform, email, setEmail, password, setPassword,
@@ -36,7 +37,7 @@ const EditPasswordModal = ({ open, setOpen,
                     <TextField type="text" id="platform" label="Platform Name" variant="standard" value={platform} onChange={e => setPlatform(e.target.value)} />
                     <TextField type="text" id="standard-basic" label="E-mail or Username" variant="standard" value={email} onChange={e => setEmail(e.target.value)} />
                     <TextField type="text" id="standard-basic" label="Password" variant="standard" value={password} onChange={e => setPassword(e.target.value)} />
-                    <TextField type="text" id="standard-basic" label="[optional] Backup codes , security code or others" variant="standard" value={optional} onChange={e => setOptional(e.target.value)} />
+                    <TextareaAutosize minRows={3} placeholder="[optional] Backup codes, Security Keys, etc... " value={optional} onChange={e => setOptional(e.target.value)} className="mt-4 outline-none border-b-4" />
                     <button disabled={loading ? true : false} className="p-2 my-2 bg-blue-700 text-white shadow-sm rounded w-full">
                         {loading ? "please wait..." : "Update"}
                     </button>
